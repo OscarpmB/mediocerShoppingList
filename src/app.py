6bed35d4b0229f85aee3ddf4d2c840ec44c6e9ad
oscar <77434966+OscarpmB@users.noreply.github.com>
2022-02-09 13:12:59 +0100

    try handle pages with seperate class

--- src/app.py
+++ src/app.py
@@ -5,75 +5,22 @@ from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.popup import Popup
 from kivy.uix.floatlayout import FloatLayout
-items = {}
+from kivy.uix.widget import Widget
 
-class p(FloatLayout):
-    pass
-
-
+class listPage(Widget):
+    def build(self):
+        self.startLayout = GridLayout(cols=1)
+        self.topLable = Label(text="Welcome")
+        self.startLayout.add_widget(self.topLable)
+        
+        return self.startLayout
 
 class MainApp(App):
-    global items
-    
     def build(self):
-        self.counter = 0 # dummy index for testing item adding and removing
-
-        self.startPage = GridLayout(cols=1) # size for base grid
-
-        self.toplable = Label(text = "Welcome!")
-        self.startPage.add_widget(self.toplable)
-        
-        # listSegment holds item list in middle of screen
-        self.listSegment = GridLayout(cols=1)
-        self.startPage.add_widget(self.listSegment)
-
-        # button to add items to list segment
-        addBtn = Button(text="Add item")
-        addBtn.bind(on_release = self.addItem)
-        self.startPage.add_widget(addBtn)
-
-        return self.startPage
-
-
-    def addItem(self, instance):
-        
-        display_popup()
-
-        i = 'carrot' + str(self.counter)
-        q = self.counter
-        self.counter = self.counter + 1
-
-        row = BoxLayout()
-        items[i] = q
-        item = Label(text=i)
-        quantity = Label(text=str(q))
-        row.add_widget(item)
-        row.add_widget(quantity)
-        
-        # Delete button
-        remove = Button(text = "remove")
-        
-        row.add_widget(remove)
-
-
-        self.listSegment.add_widget(row)
-
-        print(items)
-        return
-
-    def deleteItem(self, instance):
-        self.listSegment.remove_widget(self)
-
-def display_popup():
-    show = p()
-    popup = Popup(title='Add item', content=show, size_hint=(None, None), size=(400,400))
-    popup.open()
-    
-    print(show.item)
-         
-
-
-
+        self.startLayout = GridLayout(cols=1)
+        self.topLable = Label(text="Welcome")
+        self.startLayout.add_widget(self.topLable)
+        return self.startLayout
 
 if __name__ == '__main__':
     MainApp().run()
